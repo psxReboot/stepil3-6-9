a2157941d8d8a83e6367acce7ce0f0e54caa069c
adyubkin <adyubkin2@gmail.com>
2022-07-05 16:31:25 +0300

    All done

--- conftest.py
+++ conftest.py
@@ -1,5 +1,8 @@
+import time
+
 import pytest
 from selenium import webdriver
+import time
 
 def pytest_addoption(parser):
     parser.addoption("--language", action='store', default='ru', help="Choose language")
@@ -10,4 +13,5 @@ def browser(request):
     browser=webdriver.Chrome()
     browser.get(f"http://selenium1py.pythonanywhere.com/{browser_language}/catalogue/coders-at-work_207/")
     yield browser
+    time.sleep(5)
     browser.quit()
